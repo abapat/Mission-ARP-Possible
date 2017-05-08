0a450cd3cd7950884d7975a8af254774fc5d3ff2
Amit Bapat <amitbap@gmail.com>
2017-05-07 20:56:47 -0400

    pushing UNTESTED code

--- NetworkManager.py
+++ NetworkManager.py
@@ -10,9 +10,12 @@ import socket
 import sys
 import time
 import struct
+from scapy.all import *
 
 DHCP_PORT = 8888
 CA_PORT = 9999
+
+# Class definitions for Socket and SecureArp
 
 '''
 Wrapper over python socket module, for ease of use
@@ -20,6 +23,7 @@ Wrapper over python socket module, for ease of use
 class Socket:
     def __init__(self, ip, port, server=False, tcp=False):
         self.server = server
+        self.tcp = tcp
         if tcp:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         else:
@@ -50,31 +54,45 @@ class Socket:
     def check_for_udp_conn(self):
         pass
 
-    def recv_message(self, wait=False):
+    def __recv(self, size, blocking):
+        data = None
+        addr = None
         sock = self.sock
-        res = None
 
         if self.server:
             sock = self.conn
 
-        if wait:
+        if blocking:
             sock.settimeout(None)
         else:
             sock.settimeout(0.0)
 
         try:
-            data = sock.recv(4)
-            if len(data) < 4:
-                return None #discard
-            
-            size = struct.unpack("I", data)[0]
-            res = sock.recv(int(size))
-
+            if self.tcp:
+                data = sock.recv(size)
+                addr = sock.getsockname()
+            else:
+                data, addr = sock.recvfrom(size)
         except Exception, e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
             print("Error [line %d]: %s" % (exc_tb.tb_lineno,str(e)))
 
-        return res
+        return data, addr
+
+    def recv_message(self, wait=False):
+        data, addr = self.__recv(4, wait)
+        if not data:
+            return None
+
+        try:            
+            size = int(struct.unpack("I", data)[0])
+        except Exception, e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            print("Error unpacking data [line %d]: %s" % (exc_tb.tb_lineno,str(e)))
+
+        data, addr = self.__recv(size, wait)
+
+        return data,addr
 
     def send_message(self, msg):
         sock = self.sock
@@ -86,6 +104,90 @@ class Socket:
         length = struct.pack("I", len(msg))
         sock.send(length + msg)
 
+
+'''
+Class to handle parsing and creating SecureArp packets
+'''
+class SecureArp:
+    def __init__(self, raw=None):
+        # args = src_mac, src_ip, query_ip
+        self.arp_size = 28 # bytes
+        self.sig_size = 512
+        self.valid = False
+
+        if raw:
+            self.valid = self.__from_raw(raw)
+
+    def __from_raw(self, raw)
+        arp = raw[:self.arp_size]
+        sig = raw[self.arp_size:]
+        
+        if len(sig) > self.sig_size:
+            print("Error in parsing raw Secure Arp Packet")
+            return False
+        
+        if len(sig) == 0:
+            return False # No sig, discard
+
+        try:
+            pkt = ARP(arp)
+        except Exception, e:
+            print("Error in parsing raw Secure Arp Packet: %s" % str(e))
+            return False
+
+        self.pkt = pkt
+        if not self.validate_sig(sig):
+            return False # Bad sig, discard
+        
+        self.sig = sig
+
+        return True
+
+    def create_query(self, src_mac, src_ip, query_ip):
+        try:
+            self.pkt = ARP(op=ARP.who_has, hwsrc=src_mac, psrc=src_ip, hwdest='ff:ff:ff:ff:ff:ff', pdst=query_ip)
+        except Exception, e:
+            print("Error in creating raw Secure Arp Packet: %s" % str(e))
+            return False
+
+        self.valid = True
+        return True
+
+    # assuming packet already represents valid (parsed) query
+    def create_response(self, src_mac, src_ip):
+        if not self.valid:
+            return False
+
+        if self.pkt.op == ARP.who_has and self.pkt.pdst = src_ip: # if query is for me
+            self.pkt.op = ARP.is_at
+            self.pkt.hwdst = self.pkt.hwsrc # send back to host who sent query
+            self.pkt.hwsrc = src_mac
+            self.pkt.pdst = self.pkt.psrc
+            self.pkt.psrc = src_ip
+            # response arp is now ready, set sig
+            if self.create_sig():
+                return True
+
+        return False
+
+    def create_sig(self):
+        return "fake_sig"
+
+    def validate_sig(self, sig):
+        return True
+
+    def serialize(self):
+        if not self.valid:
+            return None
+
+        data = str(self.pkt) # should be hex string
+        data += str(self.sig) # needs to be able to send over network
+        return data
+
+
+# Modes for nodes to continuously run in
+
+
 '''
 Connect to CA, continuously check file for update and send to CA
 @arg port to bind to
@@ -96,7 +198,7 @@ def dhcp_mode():
     # loop and send updates... simple echo for now
     s.send_message("Hello")
     #time.sleep(2)
-    data = s.recv_message(wait=True)
+    data,addr = s.recv_message(wait=True) # data can be null
     print(data)
 
 '''
@@ -107,21 +209,22 @@ Listen on a port for queries
 def ca_mode(dhcp_ip):
     # Set up DHCP conn
     dhcp_sock = Socket(dhcp_ip, DHCP_PORT, tcp=True)
-    msg = dhcp_sock.recv_message(wait=True)
-    print(msg)
+    data, addr = dhcp_sock.recv_message(wait=True)
+    print(data)
     dhcp_sock.send_message("Roger")
 
     # Listen on DHCP port AND CA port for queries
     # TODO use threads
     ca_sock = Socket('', CA_PORT, server=True)
     while True:
-        data = dhcp_sock.recv_message()
+        data,addr = dhcp_sock.recv_message()
         if data:
             print("[*] Received update from DHCP")
             ca_handle_dhcp(data, dhcp_sock)
 
         if ca_sock.check_for_udp_conn():
             ca_handle_query(ca_sock) # handles query and kills conn
+
 
 def host_mode():
     pass
